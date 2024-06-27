1d72994b35dab255ab253c70774ef70d17be811e
danilo-patrucco <danilo.patrucco@sev1tech.com>
2024-06-27 10:43:40 -0400

    change

--- dockerfile
+++ dockerfile
@@ -1,1 +1,3 @@
-FROM 
+FROM registry1.dso.mil/ironbank/redhat/ubi/ubi9-minimal:9.4
+
+COPY certificates/* /etc/ssl/certs
--- download_commit_certs.py
+++ download_commit_certs.py
@@ -5,7 +5,7 @@ import requests
 import shutil
 
 # Directory setup
-repo_root_path = os.path.join(os.getcwd(), f'certificates')
+repo_root_path = os.path.join(os.getcwd(), 'certificates')
 download_dir = './downloads'
 os.makedirs(download_dir, exist_ok=True)
 os.makedirs(repo_root_path, exist_ok=True)
@@ -26,9 +26,13 @@ for url in urls:
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(download_dir)
 
-# Convert .p7b files to .pem and merge them
+# Process .p7b and .cer files in the directory
 for root, dirs, files in os.walk(download_dir):
     pem_files = []
+    # Create a meaningful name from the path
+    relative_path = root[len(download_dir):].strip(os.sep).replace(os.sep, '_')
+    identifier = relative_path.split('_')[-1]
+
     for file in files:
         if file.endswith('.p7b'):
             p7b_path = os.path.join(root, file)
@@ -37,21 +41,28 @@ for root, dirs, files in os.walk(download_dir):
             # Convert p7b to pem
             subprocess.run(['openssl', 'pkcs7', '-inform', 'DER', '-in', p7b_path,
                             '-print_certs', '-outform', 'PEM', '-out', pem_path])
+        elif file.endswith('.cer'):
+            cer_path = os.path.join(root, file)
+            pem_path = cer_path.replace('.cer', '.pem')
+            pem_files.append(pem_path)
+            # Convert cer to pem
+            subprocess.run(['openssl', 'x509', '-in', cer_path, '-out', pem_path, '-outform', 'PEM'])
 
     # Merge all pem files in the directory
     if pem_files:
-        identifier = root.split(os.sep)[-1].split('_')[-1]  # Extract the identifier from the directory name
         merged_pem_path = os.path.join(root, f'merged_certs_{identifier}.pem')
         with open(merged_pem_path, 'wb') as merged_file:
             for pem_file in pem_files:
                 with open(pem_file, 'rb') as pf:
                     merged_file.write(pf.read())
 
-        # Move merged pem file to the root of the repository
+    # Move processed files to the root of the repository
+    if pem_files:
         subprocess.run(['cp', merged_pem_path, repo_root_path])
 
 # Remove downloads dir 
 shutil.rmtree(download_dir)
+
 # Commit the changes to the local repository
 subprocess.run(['git', '-C', repo_root_path, 'add', '.'])
 subprocess.run(['git', '-C', repo_root_path, 'commit', '-m', 'Add updated PEM files'])
